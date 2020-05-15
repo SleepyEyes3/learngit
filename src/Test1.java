305765c178d1c7991a1001cbf57df090b7ae03eb
Bruce_J <jxy941112@163.com>
2020-05-16 07:07:46 +0800

    在1中的第二次提交

--- src/Test1.java
+++ src/Test1.java
@@ -3,5 +3,8 @@ public class Test1 {
         System.out.println("Hello Git");
         System.out.println("在1中添加信息！");
         System.out.println("在2中添加信息!");
+
+        System.out.println("在1中的第一次commit");
+        System.out.println("在1中的第二次commit");
     }
 }
