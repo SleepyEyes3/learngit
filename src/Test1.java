9062b9e11428779c99d2dca6e2161d5a25f7ecec
Bruce_J <jxy941112@163.com>
2020-05-16 07:02:38 +0800

    在2中添加信息！

--- src/Test1.java
+++ src/Test1.java
@@ -2,5 +2,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("在1中添加信息！");
+        System.out.println("在2中添加信息!");
     }
 }
