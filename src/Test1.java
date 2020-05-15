67edd79325b51a9c5974f058fe6956b4215b1bde
Bruce_J <jxy941112@163.com>
2020-05-16 07:39:43 +0800

    这时remote主分支已经修改了，但是2号分支在本地也进行了修改！

--- src/Test1.java
+++ src/Test1.java
@@ -3,5 +3,13 @@ public class Test1 {
         System.out.println("Hello Git");
         System.out.println("在1中添加信息！");
         System.out.println("在2中添加信息!");
+
+        System.out.println("在1中的第一次commit");
+        System.out.println("在1中的第二次commit");
+
+        System.out.println("1号分支创建");
+
+        System.out.println("2号分支创建！");
+        System.out.println("这时remote主分支已经修改了，但是2号分支在本地也进行了修改！");
     }
 }
